706cf9d36c11c69e866651979982863605973e8c
Lance Cruzado <raphaellcruzado@gmail.com>
2023-01-07 04:25:19 -0800

    moving the bar

--- src/components/input-form.js
+++ src/components/input-form.js
@@ -3,7 +3,11 @@ export default function userInput() {
   return (
     <form>
       <label>
-        <input type="number" placeholder="input binary here"/>
+        <input
+        type="number"
+        placeholder="input binary here"
+        maxLength="number8"
+        />
       </label>
     </form>
   )

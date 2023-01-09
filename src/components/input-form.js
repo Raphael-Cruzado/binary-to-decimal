a252017dbaf8ef2d03d06c5875d90565608216c2
Lance Cruzado <raphaellcruzado@gmail.com>
2023-01-08 22:49:51 -0800

    work on the button from here: submit

--- src/components/input-form.js
+++ src/components/input-form.js
@@ -1,7 +1,14 @@
 import { useState } from "react";
 
 function UserInput() {
-  const [value, setValue] = useState('');
+  let [value, setValue] = useState('');
+  const handleChange = (e) => {
+    value = e.target.value;
+    setValue(e.target.value);
+    console.log(value);
+  }
+
+  // tomorrow get the value on submit and analyze it
 
   return (
     <form>
@@ -11,7 +18,7 @@ function UserInput() {
       placeholder="input binary here"
       maxLength="8"
       value={value}
-      onChange={(e) => setValue(e.target.value)}
+      onChange={handleChange}
       />
       <br/>
       <button>Calculate</button>
